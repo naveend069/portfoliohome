a17d5759fbc67afb5199926630594155ff188d52
naveend069 <emailforpalcement@gmail.com>
2025-03-26 23:47:46 +0530

    CHANGE

--- script.js
+++ script.js
@@ -1,64 +1,79 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
+$(document).ready(function() {
+    // Sticky navbar on scroll
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
+
+        // Scroll-up button show/hide
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
+
+        // Animation on scroll for project cards
+        $('.card').each(function() {
+            var cardPos = $(this).offset().top;
+            var windowScroll = $(window).scrollTop() + $(window).height();
+            if (cardPos < windowScroll) {
+                $(this).addClass('animate__animated animate__fadeInUp'); // Add animation class
+            }
+        });
     });
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+
+    // Slide-up button click
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // Removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+
+    // Smooth scroll for menu items
+    $('.navbar .menu li a').click(function() {
         $('html').css("scrollBehavior", "smooth");
     });
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+
+    // Toggle menu/navbar
+    $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    // typing text animation script
+
+    // Typing text animation
     var typed = new Typed(".typing", {
-        strings: [ "Developer", "Designer", "Freelancer"],
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-    var typed = new Typed(".typing-2", {
-        strings: [ "Developer",  "Designer", "Freelancer"],
+
+    var typed2 = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-    // owl carousel script
+
+    // Owl carousel for project showcase
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
         autoplay: true,
-        autoplayTimeOut: 2000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
